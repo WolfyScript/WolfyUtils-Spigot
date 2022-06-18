12f178e4238ff1ebeabe2ba47fb46b085762935f
WolfyScript <wolfyscript@gmail.com>
2022-06-18 20:37:07 +0200

    Added QueryNodeIntArray

---
+++ core/src/main/java/com/wolfyscript/utilities/bukkit/nbt/QueryNodeIntArray.java
@@ -0,0 +1,32 @@
+package com.wolfyscript.utilities.bukkit.nbt;
+
+import com.fasterxml.jackson.annotation.JacksonInject;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import de.tr7zw.changeme.nbtapi.NBTCompound;
+import de.tr7zw.changeme.nbtapi.NBTType;
+import me.wolfyscript.utilities.util.NamespacedKey;
+
+import java.util.Optional;
+
+public class QueryNodeIntArray extends QueryNodePrimitive<int[]> {
+
+    public static final NamespacedKey TYPE = NamespacedKey.wolfyutilties("int_array");
+
+    @JsonCreator
+    public QueryNodeIntArray(@JsonProperty("value") int[] value, @JacksonInject("key") String key, @JacksonInject("parent_path") String parentPath) {
+        super(TYPE, value, key, parentPath);
+        this.nbtType = NBTType.NBTTagByteArray;
+    }
+
+    @Override
+    protected Optional<int[]> readValue(String path, String key, NBTCompound parent) {
+        return Optional.ofNullable(parent.getIntArray(key));
+    }
+
+    @Override
+    protected void applyValue(String path, String key, int[] value, NBTCompound resultContainer) {
+        resultContainer.setIntArray(key, value);
+    }
+
+}
