9dad980958e4d4c18601bdf20bd9062970f0edd8
WolfyScript <wolfyscript@gmail.com>
2022-07-19 23:19:34 +0200

    Fixed spawn chunks not being initialised. I could have simply set `load: STARTUP`, but that might cause unforeseen issues with compatibility.

--- core/src/main/java/com/wolfyscript/utilities/bukkit/listeners/PersistentStorageListener.java
+++ core/src/main/java/com/wolfyscript/utilities/bukkit/listeners/PersistentStorageListener.java
@@ -3,9 +3,12 @@ package com.wolfyscript.utilities.bukkit.listeners;
 import com.wolfyscript.utilities.bukkit.WolfyCoreBukkit;
 import com.wolfyscript.utilities.bukkit.persistent.PersistentStorage;
 import com.wolfyscript.utilities.bukkit.persistent.world.ChunkStorage;
+import org.bukkit.Bukkit;
 import org.bukkit.Chunk;
+import org.bukkit.World;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.server.ServerLoadEvent;
 import org.bukkit.event.world.ChunkLoadEvent;
 import org.bukkit.event.world.ChunkUnloadEvent;
 
@@ -22,9 +25,7 @@ public class PersistentStorageListener implements Listener {
     @EventHandler
     private void onChunkLoad(ChunkLoadEvent event) {
         Chunk chunk = event.getChunk();
-        ChunkStorage chunkStorage = persistentStorage.getOrCreateWorldStorage(event.getWorld()).getOrCreateChunkStorage(chunk.getX(), chunk.getZ());
-        chunkStorage.loadBlocksIntoCache();
-
+        initOrUpdateChunk(chunk);
         //TODO: Start Particle Effects
 
     }
@@ -37,6 +38,21 @@ public class PersistentStorageListener implements Listener {
         //TODO: Stop Particle Effects
 
 
+    }
+
+    @EventHandler
+    private void onServerLoad(ServerLoadEvent event) {
+        for (World world : Bukkit.getWorlds()) {
+            for (Chunk chunk : world.getLoadedChunks()) {
+                initOrUpdateChunk(chunk);
+            }
+            world.getForceLoadedChunks().forEach(this::initOrUpdateChunk);
+        }
+    }
+
+    private void initOrUpdateChunk(Chunk chunk) {
+        ChunkStorage chunkStorage = persistentStorage.getOrCreateWorldStorage(chunk.getWorld()).getOrCreateChunkStorage(chunk.getX(), chunk.getZ());
+        chunkStorage.loadBlocksIntoCache();
     }
 
 
