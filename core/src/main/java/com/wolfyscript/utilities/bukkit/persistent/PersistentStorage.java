99762b7cf04d01a29a2ae6ea219cf9b9c4a1cc01
WolfyScript <wolfyscript@gmail.com>
2022-11-11 22:41:30 +0100

    Added getOrCreate method for PlayerStorage

--- core/src/main/java/com/wolfyscript/utilities/bukkit/persistent/PersistentStorage.java
+++ core/src/main/java/com/wolfyscript/utilities/bukkit/persistent/PersistentStorage.java
@@ -1,7 +1,7 @@
 package com.wolfyscript.utilities.bukkit.persistent;
 
 import com.google.common.base.Preconditions;
-import com.wolfyscript.utilities.bukkit.WolfyCoreBukkit;
+import com.wolfyscript.utilities.bukkit.persistent.player.PlayerStorage;
 import com.wolfyscript.utilities.bukkit.persistent.world.WorldStorage;
 import me.wolfyscript.utilities.api.WolfyUtilCore;
 import org.bukkit.World;
@@ -9,11 +9,13 @@ import org.bukkit.World;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
+import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 public class PersistentStorage {
 
     private final Map<UUID, WorldStorage> WORLD_STORAGE = new HashMap<>();
+    private final Map<UUID, PlayerStorage> PLAYER_STORAGE = new HashMap<>();
     private final WolfyUtilCore core;
 
     public PersistentStorage(WolfyUtilCore core) {
@@ -23,6 +25,11 @@ public class PersistentStorage {
     public WorldStorage getOrCreateWorldStorage(@NotNull World world) {
         Preconditions.checkNotNull(world, "The world cannot be null!");
         return WORLD_STORAGE.computeIfAbsent(world.getUID(), uuid -> new WorldStorage(core, uuid));
+    }
+
+    public PlayerStorage getOrCreatePlayerStorage(@NotNull Player player) {
+        Preconditions.checkNotNull(player, "The player cannot be null!");
+        return PLAYER_STORAGE.computeIfAbsent(player.getUniqueId(), uuid -> new PlayerStorage(core, uuid));
     }
 
     public WolfyUtilCore getCore() {
