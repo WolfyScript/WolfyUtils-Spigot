4fc59f0e8477055d3f615e089e49e5c336101afd
WolfyScript <wolfyscript@gmail.com>
2022-11-22 11:17:30 +0100

    Updated BukkitItemStackConfig to use ValueProviders

--- core/src/main/java/com/wolfyscript/utilities/bukkit/items/BukkitItemStackConfig.java
+++ core/src/main/java/com/wolfyscript/utilities/bukkit/items/BukkitItemStackConfig.java
@@ -1,8 +1,15 @@
 package com.wolfyscript.utilities.bukkit.items;
 
+import com.fasterxml.jackson.annotation.JacksonInject;
 import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.wolfyscript.utilities.common.WolfyUtils;
 import com.wolfyscript.utilities.common.items.ItemStackConfig;
+import com.wolfyscript.utilities.eval.context.EvalContext;
+import com.wolfyscript.utilities.eval.operator.BoolOperatorConst;
+import com.wolfyscript.utilities.eval.value_provider.ValueProvider;
+import com.wolfyscript.utilities.eval.value_provider.ValueProviderIntegerConst;
+import com.wolfyscript.utilities.eval.value_provider.ValueProviderStringConst;
 import java.util.Map;
 import java.util.stream.Collectors;
 import org.bukkit.Material;
@@ -14,21 +21,23 @@ import org.bukkit.inventory.meta.ItemMeta;
 public class BukkitItemStackConfig extends ItemStackConfig<ItemStack> {
 
     @JsonCreator
-    public BukkitItemStackConfig(@JsonProperty("itemId") String itemId) {
-        super(itemId);
+    public BukkitItemStackConfig(@JacksonInject WolfyUtils wolfyUtils, @JsonProperty("itemId") String itemId) {
+        super(wolfyUtils, itemId);
     }
 
-    public BukkitItemStackConfig(ItemStack stack) {
-        super(stack.getType().getKey().toString());
+    public BukkitItemStackConfig(WolfyUtils wolfyUtils, ItemStack stack) {
+        super(wolfyUtils, stack.getType().getKey().toString());
 
-        this.amount = stack.getAmount();
+        this.amount = new ValueProviderIntegerConst(wolfyUtils, stack.getAmount());
         ItemMeta meta = stack.getItemMeta();
         if (meta != null) {
-            this.lore = meta.getLore();
-            this.unbreakable = meta.isUnbreakable();
-            this.customModelData = meta.getCustomModelData();
+            if (meta.hasLore()) {
+                this.lore = meta.getLore().stream().map(s -> new ValueProviderStringConst(wolfyUtils, s)).collect(Collectors.toList());
+            }
+            this.unbreakable = new BoolOperatorConst(wolfyUtils, meta.isUnbreakable());
+            this.customModelData = new ValueProviderIntegerConst(wolfyUtils, meta.getCustomModelData());
         }
-        this.enchants = stack.getEnchantments().entrySet().stream().collect(Collectors.toMap(entry-> entry.getKey().getKey().toString(), Map.Entry::getValue));
+        this.enchants = stack.getEnchantments().entrySet().stream().collect(Collectors.toMap(entry-> entry.getKey().getKey().toString(), entry -> new ValueProviderIntegerConst(wolfyUtils, entry.getValue())));
 
     }
 
@@ -37,23 +46,23 @@ public class BukkitItemStackConfig extends ItemStackConfig<ItemStack> {
         Material type = Material.matchMaterial(itemId);
         if (type != null) {
             ItemStack itemStack = new ItemStack(type);
-            itemStack.setAmount(amount);
+            itemStack.setAmount(amount.getValue());
 
             ItemMeta meta = itemStack.getItemMeta();
             if (meta != null) {
                 //TODO: Adventure format
-                meta.setDisplayName(name);
-                meta.setLore(lore);
+                meta.setDisplayName(name.getValue());
+                meta.setLore(lore.stream().map(ValueProvider::getValue).toList());
 
-                for (Map.Entry<String, Integer> entry : enchants.entrySet()) {
+                for (Map.Entry<String, ValueProvider<Integer>> entry : enchants.entrySet()) {
                     Enchantment enchant = Enchantment.getByKey(NamespacedKey.fromString(entry.getKey()));
                     if (enchant != null) {
-                        meta.addEnchant(enchant, entry.getValue(), true);
+                        meta.addEnchant(enchant, entry.getValue().getValue(), true);
                     }
                 }
 
-                meta.setCustomModelData(customModelData);
-                meta.setUnbreakable(unbreakable);
+                meta.setCustomModelData(customModelData.getValue());
+                meta.setUnbreakable(unbreakable.evaluate(new EvalContext()));
 
                 itemStack.setItemMeta(meta);
             }
