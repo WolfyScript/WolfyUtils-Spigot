a972d4039f53c25e094241674409c58d31dfe657
WolfyScript <wolfyscript@gmail.com>
2022-12-11 13:57:38 +0100

    Can no longer use CreativeModeTabs... as they are now client only.

--- core/src/main/java/com/wolfyscript/utilities/bukkit/nms/fallback/FallbackInventoryUtilImpl.java
+++ core/src/main/java/com/wolfyscript/utilities/bukkit/nms/fallback/FallbackInventoryUtilImpl.java
@@ -2,8 +2,10 @@ package com.wolfyscript.utilities.bukkit.nms.fallback;
 
 import com.wolfyscript.utilities.bukkit.nms.api.InventoryUtil;
 import com.wolfyscript.utilities.bukkit.nms.api.NMSUtil;
+import com.wolfyscript.utilities.bukkit.world.inventory.CreativeModeTab;
 import com.wolfyscript.utilities.versioning.ServerVersion;
 import org.apache.commons.lang3.NotImplementedException;
+import org.bukkit.Material;
 
 public class FallbackInventoryUtilImpl extends InventoryUtil {
 
@@ -13,6 +15,10 @@ public class FallbackInventoryUtilImpl extends InventoryUtil {
 
     @Override
     public void initItemCategories() {
-        throw new NotImplementedException("Item categories are not yet implement for " + ServerVersion.getVersion());
+        // Since 1.19.3 the server has no info about the Creative menu categories anymore!
+        // Future versions might sort the items manually, but maybe not...
+        for (Material material : Material.values()) {
+            CreativeModeTab.of("building_blocks").ifPresent(creativeModeTab -> creativeModeTab.registerMaterial(material));
+        }
     }
 }
