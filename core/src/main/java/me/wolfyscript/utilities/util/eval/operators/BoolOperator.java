6c223dd07e8156f5d196dd973345e58edac0f549
WolfyScript <wolfyscript@gmail.com>
2022-12-19 21:34:29 +0100

    Added BoolOperator.OptionalValueDeserializer

--- core/src/main/java/me/wolfyscript/utilities/util/eval/operators/BoolOperator.java
+++ core/src/main/java/me/wolfyscript/utilities/util/eval/operators/BoolOperator.java
@@ -18,14 +18,23 @@
 
 package me.wolfyscript.utilities.util.eval.operators;
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.wolfyscript.utilities.common.WolfyUtils;
+import java.io.IOException;
 import me.wolfyscript.utilities.util.NamespacedKey;
 import me.wolfyscript.utilities.util.eval.context.EvalContext;
+import me.wolfyscript.utilities.util.json.jackson.ValueDeserializer;
 import me.wolfyscript.utilities.util.json.jackson.annotations.KeyedBaseType;
+import me.wolfyscript.utilities.util.json.jackson.annotations.OptionalValueDeserializer;
 
 /**
  * An Operator that evaluates into a booleanish value.
  */
 @KeyedBaseType(baseType = me.wolfyscript.utilities.util.eval.operators.Operator.class)
+@OptionalValueDeserializer(deserializer = BoolOperator.OptionalValueDeserializer.class)
 public abstract class BoolOperator extends Operator {
 
     public BoolOperator(NamespacedKey namespacedKey) {
@@ -33,4 +42,17 @@ public abstract class BoolOperator extends Operator {
     }
 
     public abstract boolean evaluate(EvalContext context);
+
+    public static class OptionalValueDeserializer extends ValueDeserializer<BoolOperator> {
+
+        public OptionalValueDeserializer() {
+            super(BoolOperator.class);
+        }
+
+        @Override
+        public BoolOperator deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JsonProcessingException {
+            JsonNode node = p.readValueAsTree();
+            return new BoolOperatorConst(node.asBoolean());
+        }
+    }
 }
