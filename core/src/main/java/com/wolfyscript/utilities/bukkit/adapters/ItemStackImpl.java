2f266c862ae5b6e1afa2768d8785607ca44c57a5
WolfyScript <wolfyscript@gmail.com>
2023-01-03 21:38:09 +0100

    Added ItemStackImpl

---
+++ core/src/main/java/com/wolfyscript/utilities/bukkit/adapters/ItemStackImpl.java
@@ -0,0 +1,34 @@
+package com.wolfyscript.utilities.bukkit.adapters;
+
+import com.wolfyscript.utilities.NamespacedKey;
+import com.wolfyscript.utilities.bukkit.BukkitNamespacedKey;
+import com.wolfyscript.utilities.bukkit.WolfyUtilsBukkit;
+import com.wolfyscript.utilities.bukkit.world.items.BukkitItemStackConfig;
+import com.wolfyscript.utilities.common.WolfyUtils;
+import com.wolfyscript.utilities.common.adapters.ItemStack;
+import com.wolfyscript.utilities.common.items.ItemStackConfig;
+
+public class ItemStackImpl extends BukkitRefAdapter<org.bukkit.inventory.ItemStack> implements ItemStack {
+
+    private final WolfyUtils wolfyUtils;
+
+    public ItemStackImpl(WolfyUtilsBukkit wolfyUtils, org.bukkit.inventory.ItemStack bukkitRef) {
+        super(bukkitRef);
+        this.wolfyUtils = wolfyUtils;
+    }
+
+    @Override
+    public NamespacedKey getItem() {
+        return BukkitNamespacedKey.fromBukkit(getBukkitRef().getType().getKey());
+    }
+
+    @Override
+    public int getAmount() {
+        return getBukkitRef().getAmount();
+    }
+
+    @Override
+    public ItemStackConfig<?> snapshot() {
+        return new BukkitItemStackConfig(wolfyUtils, getBukkitRef());
+    }
+}
