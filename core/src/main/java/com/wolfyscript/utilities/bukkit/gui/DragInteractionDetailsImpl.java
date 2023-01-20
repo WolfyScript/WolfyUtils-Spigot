33a33fcd397a6a54683b9c022545be85b6b7b12e
WolfyScript <wolfyscript@gmail.com>
2023-01-20 17:51:42 +0100

    Added DragInteractionDetailsImpl

---
+++ core/src/main/java/com/wolfyscript/utilities/bukkit/gui/DragInteractionDetailsImpl.java
@@ -0,0 +1,45 @@
+package com.wolfyscript.utilities.bukkit.gui;
+
+import com.wolfyscript.utilities.common.gui.Data;
+import com.wolfyscript.utilities.common.gui.DragInteractionDetails;
+import com.wolfyscript.utilities.common.gui.DragType;
+import com.wolfyscript.utilities.common.gui.InteractionResult;
+import java.util.Set;
+import org.bukkit.event.inventory.InventoryDragEvent;
+
+public class DragInteractionDetailsImpl<D extends Data> implements DragInteractionDetails<D> {
+
+    private final InventoryDragEvent event;
+
+    public DragInteractionDetailsImpl(InventoryDragEvent event) {
+        this.event = event;
+    }
+
+    @Override
+    public Set<Integer> getInventorySlots() {
+        return event.getInventorySlots();
+    }
+
+    @Override
+    public Set<Integer> getRawSlots() {
+        return event.getRawSlots();
+    }
+
+    @Override
+    public DragType getType() {
+        return switch (event.getType()) {
+            case EVEN -> DragType.EVEN;
+            case SINGLE -> DragType.SINGLE;
+        };
+    }
+
+    @Override
+    public boolean isCancelled() {
+        return event.isCancelled();
+    }
+
+    @Override
+    public InteractionResult.ResultType getResultType() {
+        return null;
+    }
+}
