e638622b94432de54c37d7ab8b4478972adb0da4
WolfyScript <wolfyscript@gmail.com>
2023-04-10 22:35:53 +0200

    Add TagResolver to ButtonState dynamic icon that resolves signal values.

--- core/src/main/java/com/wolfyscript/utilities/bukkit/gui/ButtonStateImpl.java
+++ core/src/main/java/com/wolfyscript/utilities/bukkit/gui/ButtonStateImpl.java
@@ -1,13 +1,17 @@
 package com.wolfyscript.utilities.bukkit.gui;
 
 import com.google.inject.Inject;
+import com.wolfyscript.utilities.bukkit.WolfyCoreBukkit;
 import com.wolfyscript.utilities.bukkit.eval.context.EvalContextPlayer;
+import com.wolfyscript.utilities.bukkit.world.items.BukkitItemStackConfig;
 import com.wolfyscript.utilities.common.gui.Button;
 import com.wolfyscript.utilities.common.gui.ButtonComponentState;
 import com.wolfyscript.utilities.common.gui.ComponentState;
 import com.wolfyscript.utilities.common.gui.GuiHolder;
 import com.wolfyscript.utilities.common.gui.RenderContext;
-import org.bukkit.inventory.ItemStack;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.tag.Tag;
+import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
 
 public class ButtonStateImpl extends ComponentStateImpl<Button, ComponentState> implements ButtonComponentState {
 
@@ -22,7 +26,26 @@ public class ButtonStateImpl extends ComponentStateImpl<Button, ComponentState> 
         if (getOwner().icon() instanceof ButtonImpl.StaticIcon staticIcon) {
             renderContext.setNativeStack(renderContext.getSlotOffsetToParent(), staticIcon.getStaticStack());
         } else {
-            renderContext.setNativeStack(renderContext.getSlotOffsetToParent(), (ItemStack) getOwner().icon().getStack().constructItemStack(new EvalContextPlayer(((GUIHolder) holder).getPlayer())));
+            renderContext.setNativeStack(renderContext.getSlotOffsetToParent(),
+                    ((BukkitItemStackConfig) getOwner().icon().getStack()).constructItemStack(
+                            new EvalContextPlayer(((GUIHolder) holder).getPlayer()),
+                            WolfyCoreBukkit.getInstance().getWolfyUtils().getChat().getMiniMessage(),
+                            TagResolver.resolver("signal", (argumentQueue, context1) -> {
+                                String key = argumentQueue.popOr("Missing signal id!").value();
+                                int lvlUp = 0;
+                                if (argumentQueue.hasNext()) {
+                                    lvlUp = argumentQueue.pop().asInt().orElse(0);
+                                }
+                                ComponentState state = this;
+                                for (int i = 0; i < lvlUp; i++) {
+                                    if (state.getParent() == null) break;
+                                    state = state.getParent();
+                                }
+                                Object value = state.captureSignal(key).get();
+                                return Tag.inserting(value == null ? Component.text("null") : context1.deserialize(value.toString()));
+                            })
+                    )
+            );
         }
     }
 
