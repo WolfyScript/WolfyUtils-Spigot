d8a33349fa5d7e2135c60a34dcb013477d57e837
WolfyScript <wolfyscript@gmail.com>
2023-04-10 22:37:38 +0200

    Make a simple Counter GUI with reset button
    A simple test GUI that contains a click counter. The counter is increased by one button and can be reset to 0 by another button.

--- core/src/main/java/com/wolfyscript/utilities/bukkit/gui/TestGUI.java
+++ core/src/main/java/com/wolfyscript/utilities/bukkit/gui/TestGUI.java
@@ -4,6 +4,7 @@ import com.wolfyscript.utilities.bukkit.WolfyCoreBukkit;
 import com.wolfyscript.utilities.bukkit.world.items.BukkitItemStackConfig;
 import com.wolfyscript.utilities.common.gui.GuiAPIManager;
 import com.wolfyscript.utilities.common.gui.InteractionResult;
+import com.wolfyscript.utilities.common.gui.Signal;
 import com.wolfyscript.utilities.common.gui.StateHook;
 import com.wolfyscript.utilities.common.gui.WindowType;
 import com.wolfyscript.utilities.eval.value_provider.ValueProviderStringConst;
@@ -23,40 +24,51 @@ public class TestGUI {
 
         MiniMessage miniMessage = core.getWolfyUtils().getChat().getMiniMessage();
 
+        final String COUNT = "count";
+
         manager.registerRouter("main", builder -> builder
                 .children(children -> children
                         .window("mainmenu", mainMenu -> mainMenu
                                 .title((guiHolder, window) -> Component.text("Main Menu"))
                                 .size(27)
+                                .useSignal(COUNT, Integer.class, count -> count.defaultValue(state -> 0))
                                 .render((guiHolder, state) -> {
-                                   // Place child components into the GUI
-                                    state.setComponent(13, "settings"); // references the registered child component with the id "settings"
+                                    // Place child components into the GUI
+                                    Signal.Value<Integer> count = state.captureSignal(COUNT, Integer.class);
+                                    state.setComponent(13, "counter"); // references the registered child component with the id "settings"
+                                    if (count.get() > 0) {
+                                        state.setComponent(20, "reset");
+                                    }
                                 })
                                 .children(mainMenuChildren -> mainMenuChildren
-                                        .button("settings", settingsBtn -> settingsBtn
+                                        .button("counter", settingsBtn -> settingsBtn
                                                 .icon(icon -> icon
                                                         .stack(() -> {
                                                             BukkitItemStackConfig config = new BukkitItemStackConfig(core.getWolfyUtils(), "minecraft:redstone");
-                                                            config.setName(new ValueProviderStringConst(core.getWolfyUtils(), "Clicked <state:count> times!"));
+                                                            config.setName(new ValueProviderStringConst(core.getWolfyUtils(), "Clicked <signal:count:1> times!"));
                                                             return config;
                                                         })
                                                         .dynamic()
                                                 )
                                                 .interact((guiHolder, componentState, interactionDetails) -> {
-                                                    StateHook<Integer> count = componentState.getOrCreateHook("count", () -> 0);
-                                                    count.set(integer -> ++integer);
-                                                    // Handle interaction
+                                                    Signal.Value<Integer> count = componentState.getParent().captureSignal(COUNT, Integer.class);
+                                                    count.update(integer -> ++integer);
+                                                    return InteractionResult.cancel(true);
+                                                })
+                                        )
+                                        .button("reset", resetBtn -> resetBtn
+                                                .icon(icon -> icon.stack(() -> {
+                                                    BukkitItemStackConfig config = new BukkitItemStackConfig(core.getWolfyUtils(), "minecraft:red_concrete");
+                                                    config.setName(new ValueProviderStringConst(core.getWolfyUtils(), "Reset Clicks!"));
+                                                    return config;
+                                                }))
+                                                .interact((guiHolder, componentState, interactionDetails) -> {
+                                                    Signal.Value<Integer> count = componentState.getParent().captureSignal(COUNT, Integer.class);
+                                                    count.update(0);
                                                     return InteractionResult.cancel(true);
                                                 })
                                         )
                                 )
-                        )
-                        .window("settings", settings -> settings
-                                .title((guiHolder, window) -> Component.text("Settings"))
-                                .type(WindowType.HOPPER)
-                                .render((guiHolder, WindowState) -> {
-                                    // Render components into the GUI
-                                })
                         )
                 )
         );
