3f30409f9dc3ae47a4234321349612b964db7892
WolfyScript <wolfyscript@gmail.com>
2023-04-12 22:48:15 +0200

    Remove ComponentBuilder
    Replaced by ComponentBuilder interface in common package

--- core/src/main/java/com/wolfyscript/utilities/bukkit/gui/ComponentBuilder.java
+++
@@ -1,8 +0,0 @@
-package com.wolfyscript.utilities.bukkit.gui;
-
-import com.wolfyscript.utilities.common.gui.Component;
-
-public interface ComponentBuilder<COMPONENT extends Component, PARENT extends Component> {
-
-    COMPONENT create(PARENT parent);
-}
