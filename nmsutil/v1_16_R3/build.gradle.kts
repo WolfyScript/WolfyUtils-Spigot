192dda5ebb16306bb35aef2287dca289ffbec8cb
WolfyScript <wolfyscript@gmail.com>
2023-10-05 20:50:27 +0200

    Use BuildTools Gradle Plugin
    * This allows for easier NMS updates
    * NMS versions can now be automatically installed, no need to manually install buildtools anymore

--- nmsutil/v1_16_R3/build.gradle.kts
+++ nmsutil/v1_16_R3/build.gradle.kts
@@ -1,7 +1,11 @@
+import com.wolfyscript.devtools.buildtools.BuildToolsInstallTask
+
 description = "v1_16_R3"
+private val mcVersion = "1.16.5"
 
 plugins {
     id("com.wolfyscript.wolfyutils.spigot.java-conventions")
+    id("com.wolfyscript.devtools.buildtools") version ("2.0-SNAPSHOT")
 }
 
 dependencies {
@@ -9,5 +13,16 @@ dependencies {
     compileOnly(project(":core"))
 }
 
-tasks {
+buildTools {
+    parent?.ext?.let {
+        buildToolsDir.set(file(it.get("buildToolsDir").toString()))
+        buildToolsJar.set(file(it.get("buildToolsJar").toString()))
+    }
+    minecraftVersion.set(mcVersion)
+}
+
+tasks.named<BuildToolsInstallTask>("prepareNMS") {
+    javaLauncher.set(javaToolchains.launcherFor {
+        languageVersion.set(JavaLanguageVersion.of(16))
+    })
 }
--- nmsutil/v1_19_R1/build.gradle.kts
+++ nmsutil/v1_19_R1/build.gradle.kts
@@ -1,8 +1,12 @@
+import com.wolfyscript.devtools.buildtools.BuildToolsInstallTask
+
 description = "v1_19_R1"
+private val mcVersion = "1.19"
 
 plugins {
     id("com.wolfyscript.wolfyutils.spigot.java-conventions")
     id("io.github.patrick.remapper") version "1.4.0"
+    id("com.wolfyscript.devtools.buildtools") version ("2.0-SNAPSHOT")
 }
 
 dependencies {
@@ -10,9 +14,17 @@ dependencies {
     compileOnly(project(":core"))
 }
 
+buildTools {
+    parent?.ext?.let {
+        buildToolsDir.set(file(it.get("buildToolsDir").toString()))
+        buildToolsJar.set(file(it.get("buildToolsJar").toString()))
+    }
+    minecraftVersion.set(mcVersion)
+}
+
 tasks {
     remap {
-        version.set("1.19")
+        version.set(mcVersion)
         dependsOn("jar")
     }
     jar {
