119cd439be7e1773ec10c8c9f8bccc6d27bb172b
WolfyScript <wolfyscript@gmail.com>
2023-10-13 23:50:07 +0200

    Make StackIdentifierParser Comparable
    (cherry picked from commit 5c99e6ba2610182d0289defaae6ad97d6911ff8d)

--- core/src/main/java/com/wolfyscript/utilities/bukkit/world/items/reference/StackIdentifierParser.java
+++ core/src/main/java/com/wolfyscript/utilities/bukkit/world/items/reference/StackIdentifierParser.java
@@ -1,14 +1,21 @@
 package com.wolfyscript.utilities.bukkit.world.items.reference;
 
+import me.wolfyscript.utilities.api.inventory.custom_items.references.APIReference;
 import me.wolfyscript.utilities.util.Keyed;
 import org.bukkit.inventory.ItemStack;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Optional;
 
-public interface StackIdentifierParser<T extends StackIdentifier> extends Keyed {
+public interface StackIdentifierParser<T extends StackIdentifier> extends Keyed, Comparable<StackIdentifierParser<T>> {
 
     int priority();
 
     Optional<T> from(ItemStack itemStack);
+
+    @Override
+    default int compareTo(@NotNull StackIdentifierParser<T> that) {
+        return Integer.compare(this.priority(), that.priority());
+    }
 
 }
