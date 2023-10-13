c89487597d4c620305fbbc800ad3915cb5d300a9
WolfyScript <wolfyscript@gmail.com>
2023-10-13 20:18:10 +0200

    Add convert method to StackIdentifier

--- core/src/main/java/com/wolfyscript/utilities/bukkit/world/items/reference/StackIdentifier.java
+++ core/src/main/java/com/wolfyscript/utilities/bukkit/world/items/reference/StackIdentifier.java
@@ -1,7 +1,7 @@
 package com.wolfyscript.utilities.bukkit.world.items.reference;
 
-import com.fasterxml.jackson.annotation.JsonGetter;
-import com.fasterxml.jackson.annotation.JsonIgnore;
+import me.wolfyscript.utilities.api.inventory.custom_items.references.APIReference;
+import me.wolfyscript.utilities.api.inventory.custom_items.references.VanillaRef;
 import me.wolfyscript.utilities.util.Keyed;
 import me.wolfyscript.utilities.util.NamespacedKey;
 import org.bukkit.World;
@@ -22,4 +22,25 @@ public interface StackIdentifier extends Keyed {
 
     @Override
     NamespacedKey getNamespacedKey();
+
+    /**
+     * Used for backwards compatibility with {@link APIReference}s.
+     * By default, this returns a {@link VanillaRef} build using the {@link #item()} method.
+     * Other implementations may choose to return their old related APIReference implementation.
+     * <p>
+     * Note that the new Identifiers are one level lower than APIReferences, so they no longer share the weight, and amount properties.
+     * Those properties are now part of the higher level {@link StackReference}.
+     * </p>
+     *
+     * @param weight The weight of the StackReference
+     * @param amount The amount of the StackReference
+     * @return The old APIReference, either implementation specific, or {@link VanillaRef} by default
+     */
+    @Deprecated
+    default APIReference convert(double weight, int amount) {
+        var ref = new VanillaRef(item());
+        ref.setWeight(weight);
+        ref.setAmount(amount);
+        return ref;
+    }
 }
