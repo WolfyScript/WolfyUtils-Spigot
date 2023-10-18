f0a7dbf703b3bb894fd23edc714a757666b048ae
WolfyScript <wolfyscript@gmail.com>
2023-10-18 22:53:36 +0200

    No longer get parser from StackIdentifier
    (cherry picked from commit bec613d321f2f6992f6f08ca23986518b376239d)

--- core/src/main/java/com/wolfyscript/utilities/bukkit/world/items/reference/StackReference.java
+++ core/src/main/java/com/wolfyscript/utilities/bukkit/world/items/reference/StackReference.java
@@ -59,7 +59,7 @@ public class StackReference implements Copyable<StackReference> {
         this.customAmount = customAmount;
         this.weight = weight;
         this.core = core;
-        this.parser = identifier.parser();
+        this.parser = core.getRegistries().getStackIdentifierParsers().get(identifier.getNamespacedKey());
         this.stack = item;
         this.identifier = identifier;
     }
