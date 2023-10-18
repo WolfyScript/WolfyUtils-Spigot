b1a7689eacde6f4325cc739d5bfd32901de78c96
WolfyScript <wolfyscript@gmail.com>
2023-10-18 22:55:05 +0200

    Add RegistryStackIdentifierParsers#parseFrom
    (cherry picked from commit 246543aa24fd4cc4a6eada3101b7a0245c24c31f)

--- core/src/main/java/com/wolfyscript/utilities/bukkit/registry/RegistryStackIdentifierParsers.java
+++ core/src/main/java/com/wolfyscript/utilities/bukkit/registry/RegistryStackIdentifierParsers.java
@@ -1,21 +1,32 @@
 package com.wolfyscript.utilities.bukkit.registry;
 
 import com.google.common.base.Preconditions;
+import com.wolfyscript.utilities.NamespacedKey;
+import com.wolfyscript.utilities.bukkit.BukkitNamespacedKey;
+import com.wolfyscript.utilities.bukkit.world.items.reference.BukkitStackIdentifier;
+import com.wolfyscript.utilities.bukkit.world.items.reference.StackIdentifier;
 import com.wolfyscript.utilities.bukkit.world.items.reference.StackIdentifierParser;
+import com.wolfyscript.utilities.bukkit.world.items.reference.StackReference;
+import com.wolfyscript.utilities.common.registry.Registries;
+import com.wolfyscript.utilities.common.registry.RegistrySimple;
 import me.wolfyscript.utilities.registry.Registries;
 import me.wolfyscript.utilities.registry.RegistrySimple;
 import me.wolfyscript.utilities.util.NamespacedKey;
 import me.wolfyscript.utilities.util.particles.ParticleAnimation;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 public class RegistryStackIdentifierParsers extends RegistrySimple<StackIdentifierParser<?>> {
 
     private List<StackIdentifierParser<?>> priorityIndexedParsers = List.of();
+    private final Registries registries;
 
     public RegistryStackIdentifierParsers(Registries registries) {
-        super(new NamespacedKey(registries.getCore(), "stack_identifier/parsers"), registries, (Class<StackIdentifierParser<?>>)(Object) StackIdentifierParser.class);
+        super(new BukkitNamespacedKey(registries.getCore(), "stack_identifier/parsers"), registries, (Class<StackIdentifierParser<?>>)(Object) StackIdentifierParser.class);
+        this.registries = registries;
     }
 
     @Override
@@ -29,6 +40,24 @@ public class RegistryStackIdentifierParsers extends RegistrySimple<StackIdentifi
 
     public List<StackIdentifierParser<?>> sortedParsers() {
         return priorityIndexedParsers;
+    }
+
+    public StackIdentifier parseIdentifier(ItemStack stack) {
+        for (StackIdentifierParser<?> parser : sortedParsers()) {
+            Optional<? extends StackIdentifier> identifierOptional = parser.from(stack);
+            if (identifierOptional.isPresent()) return identifierOptional.get();
+        }
+        return new BukkitStackIdentifier(stack);
+    }
+
+    /**
+     *
+     *
+     * @param stack
+     * @return
+     */
+    public StackReference parseFrom(ItemStack stack) {
+        return new StackReference(registries.getCore(), parseIdentifier(stack), 1, 1, stack);
     }
 
     private void reIndexParsers() {
