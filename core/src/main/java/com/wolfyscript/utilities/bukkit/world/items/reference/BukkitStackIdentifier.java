3614626ed488ace2d7ff5e3ff8265ca9387a5355
WolfyScript <wolfyscript@gmail.com>
2023-10-25 22:23:37 +0200

    Add display option for BukkitStackIdentifier

--- core/src/main/java/com/wolfyscript/utilities/bukkit/world/items/reference/BukkitStackIdentifier.java
+++ core/src/main/java/com/wolfyscript/utilities/bukkit/world/items/reference/BukkitStackIdentifier.java
@@ -55,6 +55,14 @@ public class BukkitStackIdentifier implements StackIdentifier {
         public NamespacedKey getNamespacedKey() {
             return ID;
         }
+
+        @Override
+        public DisplayConfiguration displayConfig() {
+            return new DisplayConfiguration.SimpleDisplayConfig(
+                    Component.text("Bukkit").color(NamedTextColor.WHITE).decorate(TextDecoration.BOLD),
+                    new DisplayConfiguration.MaterialIconSettings(Material.LAVA_BUCKET)
+            );
+        }
     }
 
 }
