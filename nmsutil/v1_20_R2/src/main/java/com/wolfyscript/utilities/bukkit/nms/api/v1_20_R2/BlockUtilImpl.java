d94f31773c9358dcad185c06597a6f43ba532008
WolfyScript <wolfyscript@gmail.com>
2023-11-20 19:43:43 +0100

    Fix BlockUtilImpl in nms 1_20_R2

--- nmsutil/v1_20_R2/src/main/java/com/wolfyscript/utilities/bukkit/nms/api/v1_20_R2/BlockUtilImpl.java
+++ nmsutil/v1_20_R2/src/main/java/com/wolfyscript/utilities/bukkit/nms/api/v1_20_R2/BlockUtilImpl.java
@@ -16,11 +16,11 @@
  *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
 
-package me.wolfyscript.utilities.api.nms.v1_20_R2;
+package com.wolfyscript.utilities.bukkit.nms.api.v1_20_R2;
 
-import me.wolfyscript.utilities.api.nms.BlockUtil;
-import me.wolfyscript.utilities.api.nms.NMSUtil;
-import me.wolfyscript.utilities.api.nms.v1_20_R2.block.NMSBrewingStand;
+import com.wolfyscript.utilities.bukkit.nms.api.BlockUtil;
+import com.wolfyscript.utilities.bukkit.nms.api.NMSUtil;
+import com.wolfyscript.utilities.bukkit.nms.api.v1_20_R2.block.NMSBrewingStand;
 import org.bukkit.block.BrewingStand;
 
 public class BlockUtilImpl extends BlockUtil {
