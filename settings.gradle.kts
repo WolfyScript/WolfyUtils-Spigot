aa6a119ba5cf7f8e0ca45719af1aa56dd848cdd7
WolfyScript <wolfyscript@gmail.com>
2023-12-13 13:39:44 +0100

    Bump nbtapi-api & nbtapi-data to 2.12.2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -56,8 +56,8 @@ dependencyResolutionManagement {
             library("fastutil", "it.unimi.dsi", "fastutil").version("8.5.6")
             library("typesafe.config", "com.typesafe", "config").version("1.3.1")
             library("bstats", "org.bstats", "bstats-bukkit").version("3.0.0")
-            library("nbtapi-api", "de.tr7zw", "item-nbt-api").version("2.12.0")
-            library("nbtapi-data", "de.tr7zw", "nbt-data-api").version("2.12.0")
+            library("nbtapi-api", "de.tr7zw", "item-nbt-api").version("2.12.2")
+            library("nbtapi-data", "de.tr7zw", "nbt-data-api").version("2.12.2")
         }
         // Libraries only used for testing
         create("testLibs") {
