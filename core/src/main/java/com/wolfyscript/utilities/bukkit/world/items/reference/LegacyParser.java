3515b4532877ecbebcdee904e8f9e2bbd6fb419c
WolfyScript <wolfyscript@gmail.com>
2024-01-08 19:13:14 +0100

    Add Javadocs to LegacyParser

--- core/src/main/java/com/wolfyscript/utilities/bukkit/world/items/reference/LegacyParser.java
+++ core/src/main/java/com/wolfyscript/utilities/bukkit/world/items/reference/LegacyParser.java
@@ -4,6 +4,11 @@ import com.fasterxml.jackson.databind.JsonNode;
 
 import java.util.Optional;
 
+/**
+ * Marks a legacy StackIdentifier Parser, that parses the StackIdentifier from the old {@link me.wolfyscript.utilities.api.inventory.custom_items.references.APIReference} data "format".
+ *
+ * @param <T> The type of the StackIdentifier
+ */
 public interface LegacyParser<T extends StackIdentifier> {
 
     Optional<T> from(JsonNode legacyData);
