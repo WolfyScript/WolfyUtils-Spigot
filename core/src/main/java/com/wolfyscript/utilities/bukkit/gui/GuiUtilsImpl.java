82ed2b006e2ce0f2cb577a4e09b8c91640086ec8
WolfyScript <wolfyscript@gmail.com>
2024-03-17 16:07:06 +0100

    Remove old RenderContext factory method

--- core/src/main/java/com/wolfyscript/utilities/bukkit/gui/GuiUtilsImpl.java
+++ core/src/main/java/com/wolfyscript/utilities/bukkit/gui/GuiUtilsImpl.java
@@ -4,18 +4,11 @@ import com.wolfyscript.utilities.bukkit.gui.rendering.InventoryGUIRenderer;
 import com.wolfyscript.utilities.gui.*;
 import com.wolfyscript.utilities.gui.rendering.Renderer;
 import com.wolfyscript.utilities.platform.gui.GuiUtils;
-import java.util.UUID;
 
 public class GuiUtilsImpl implements GuiUtils {
 
     @Override
-    public RenderContext createRenderContext(Window window, ViewRuntime viewManager, UUID viewer) {
-        return null;
-    }
-
-    @Override
-    public Renderer createRenderer(ViewRuntime viewRuntime) {
-
-        return new InventoryGUIRenderer(viewRuntime);
+    public Renderer<?> createRenderer(ViewRuntime viewRuntime) {
+        return new InventoryGUIRenderer((ViewRuntimeImpl) viewRuntime);
     }
 }
