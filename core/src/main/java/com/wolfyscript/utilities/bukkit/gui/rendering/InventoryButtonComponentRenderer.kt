d1b86ca0234224a93bc6d57ae40b0ef9a7643ab2
WolfyScript <wolfyscript@gmail.com>
2024-03-17 16:10:49 +0100

    Add renderer for ButtonComponents

--- core/src/main/java/com/wolfyscript/utilities/bukkit/gui/rendering/InventoryButtonComponentRenderer.kt
+++ core/src/main/java/com/wolfyscript/utilities/bukkit/gui/rendering/InventoryButtonComponentRenderer.kt
@@ -2,9 +2,12 @@ package com.wolfyscript.utilities.bukkit.gui.rendering
 
 import com.wolfyscript.utilities.NamespacedKey
 import com.wolfyscript.utilities.bukkit.BukkitNamespacedKey
+import com.wolfyscript.utilities.eval.context.EvalContext
+import com.wolfyscript.utilities.gui.ItemStackContext
 import com.wolfyscript.utilities.gui.components.Button
 import com.wolfyscript.utilities.gui.rendering.ComponentRenderer
-import com.wolfyscript.utilities.gui.rendering.PropertyPosition
+import net.kyori.adventure.text.minimessage.MiniMessage
+import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver
 
 class InventoryButtonComponentRenderer : ComponentRenderer<Button, InvGUIRenderContext> {
 
@@ -12,7 +15,15 @@ class InventoryButtonComponentRenderer : ComponentRenderer<Button, InvGUIRenderC
 
     override fun render(context: InvGUIRenderContext, component: Button) {
 
+        context.renderer.renderStack(context.currentOffset(), component.icon().stack, object: ItemStackContext {
 
+            override fun resolvers(): TagResolver = component.icon().resolvers
+
+            override fun miniMessage(): MiniMessage = context.renderer.runtime.wolfyUtils.chat.miniMessage
+
+            override fun evalContext(): EvalContext = EvalContext()
+
+        })
 
     }
 }
