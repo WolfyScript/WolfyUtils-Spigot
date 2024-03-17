62ebd5b4b5512974fb5099657f4a1d7e89afeb1d
WolfyScript <wolfyscript@gmail.com>
2024-03-17 16:11:23 +0100

    Update InvGUIRenderContext to store the slot offset

--- core/src/main/java/com/wolfyscript/utilities/bukkit/gui/rendering/InvGUIRenderContext.kt
+++ core/src/main/java/com/wolfyscript/utilities/bukkit/gui/rendering/InvGUIRenderContext.kt
@@ -1,28 +1,11 @@
 package com.wolfyscript.utilities.bukkit.gui.rendering
 
-import com.wolfyscript.utilities.bukkit.adapters.PlayerImpl
-import com.wolfyscript.utilities.bukkit.eval.context.EvalContextPlayer
-import com.wolfyscript.utilities.eval.context.EvalContext
 import com.wolfyscript.utilities.gui.Component
-import com.wolfyscript.utilities.gui.GuiHolder
-import com.wolfyscript.utilities.gui.ItemStackContext
-import com.wolfyscript.utilities.gui.Position
 import com.wolfyscript.utilities.gui.rendering.RenderContext
-import net.kyori.adventure.text.minimessage.MiniMessage
-import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver
 
-class InvGUIRenderContext(private val renderer: InventoryGUIRenderer) : RenderContext {
+class InvGUIRenderContext(val renderer: InventoryGUIRenderer) : RenderContext {
     private var currentNode: Component? = null
     private var slotOffsetToParent = 0
-    private lateinit var currentPosition: Position
-
-    fun offset(position: Position) {
-        currentPosition = position
-    }
-
-    fun offset() : Position {
-        return currentPosition
-    }
 
     fun setSlotOffset(offset: Int) {
         this.slotOffsetToParent = offset
@@ -34,7 +17,6 @@ class InvGUIRenderContext(private val renderer: InventoryGUIRenderer) : RenderCo
 
     override fun enterNode(component: Component) {
         this.currentNode = component
-        this.slotOffsetToParent = component.offset()
     }
 
     override fun exitNode() {
@@ -43,25 +25,5 @@ class InvGUIRenderContext(private val renderer: InventoryGUIRenderer) : RenderCo
 
     override fun getCurrentComponent(): Component? {
         return currentNode
-    }
-
-    fun createContext(guiHolder: GuiHolder, tagResolvers: TagResolver): ItemStackContext {
-        return object : ItemStackContext {
-            override fun resolvers(): TagResolver {
-                return tagResolvers
-            }
-
-            override fun miniMessage(): MiniMessage {
-                return renderer.runtime.wolfyUtils.chat.miniMessage
-            }
-
-            override fun evalContext(): EvalContext {
-                return EvalContextPlayer((guiHolder.player as PlayerImpl).bukkitRef)
-            }
-
-            override fun holder(): GuiHolder {
-                return guiHolder
-            }
-        }
     }
 }
