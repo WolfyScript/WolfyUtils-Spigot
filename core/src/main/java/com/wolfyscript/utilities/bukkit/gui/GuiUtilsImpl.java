b55d1e592e38e8773797e00ea2b002ca5545c775
WolfyScript <wolfyscript@gmail.com>
2024-03-21 11:26:37 +0100

    Implement GuiUtils#createInteractionHandler

--- core/src/main/java/com/wolfyscript/utilities/bukkit/gui/GuiUtilsImpl.java
+++ core/src/main/java/com/wolfyscript/utilities/bukkit/gui/GuiUtilsImpl.java
@@ -1,7 +1,9 @@
 package com.wolfyscript.utilities.bukkit.gui;
 
+import com.wolfyscript.utilities.bukkit.gui.interaction.InventoryGUIInteractionHandler;
 import com.wolfyscript.utilities.bukkit.gui.rendering.InventoryGUIRenderer;
 import com.wolfyscript.utilities.gui.*;
+import com.wolfyscript.utilities.gui.interaction.InteractionHandler;
 import com.wolfyscript.utilities.gui.rendering.Renderer;
 import com.wolfyscript.utilities.platform.gui.GuiUtils;
 
@@ -10,5 +12,10 @@ public class GuiUtilsImpl implements GuiUtils {
     @Override
     public Renderer<?> createRenderer(ViewRuntime viewRuntime) {
         return new InventoryGUIRenderer((ViewRuntimeImpl) viewRuntime);
+    }
+
+    @Override
+    public InteractionHandler createInteractionHandler(ViewRuntime viewRuntime) {
+        return new InventoryGUIInteractionHandler((ViewRuntimeImpl) viewRuntime);
     }
 }
