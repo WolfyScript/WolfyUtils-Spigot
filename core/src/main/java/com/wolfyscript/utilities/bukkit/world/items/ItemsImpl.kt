00ab827a0fc42aa62ebda757da01045fc2549eac
WolfyScript <wolfyscript@gmail.com>
2024-04-04 14:36:36 +0200

    Convert & Update ItemsImpl

--- core/src/main/java/com/wolfyscript/utilities/bukkit/world/items/ItemsImpl.kt
+++ core/src/main/java/com/wolfyscript/utilities/bukkit/world/items/ItemsImpl.kt
@@ -1,30 +1,17 @@
-package com.wolfyscript.utilities.bukkit.world.items;
+package com.wolfyscript.utilities.bukkit.world.items
 
-import com.wolfyscript.utilities.WolfyCore;
-import com.wolfyscript.utilities.WolfyUtils;
-import com.wolfyscript.utilities.bukkit.data.SpigotDataKeyBuilderProvider;
-import com.wolfyscript.utilities.data.DataKeyBuilderProvider;
-import com.wolfyscript.utilities.platform.world.items.Items;
-import com.wolfyscript.utilities.world.items.ItemStackConfig;
+import com.wolfyscript.utilities.WolfyCore
+import com.wolfyscript.utilities.WolfyUtils
+import com.wolfyscript.utilities.bukkit.data.SpigotDataKeyBuilderProvider
+import com.wolfyscript.utilities.platform.world.items.Items
+import com.wolfyscript.utilities.world.items.ItemStackConfig
 
-public class ItemsImpl implements Items {
+class ItemsImpl(wolfyCore: WolfyCore) : Items {
 
-    private final WolfyCore wolfyCore;
-    private final SpigotDataKeyBuilderProvider dataKeyBuilderFactory;
+    override val dataKeyBuilderProvider: SpigotDataKeyBuilderProvider = SpigotDataKeyBuilderProvider(wolfyCore)
 
-    public ItemsImpl(WolfyCore wolfyCore) {
-        this.wolfyCore = wolfyCore;
-        this.dataKeyBuilderFactory = new SpigotDataKeyBuilderProvider(wolfyCore);
-    }
-
-    @Override
-    public ItemStackConfig createStackConfig(WolfyUtils wolfyUtils, String s) {
-        return new BukkitItemStackConfig(wolfyUtils, s);
-    }
-
-    @Override
-    public DataKeyBuilderProvider dataKeyProvider() {
-        return dataKeyBuilderFactory;
+    override fun createStackConfig(wolfyUtils: WolfyUtils, itemId: String): ItemStackConfig {
+        return BukkitItemStackConfig(wolfyUtils, itemId)
     }
 
 }
