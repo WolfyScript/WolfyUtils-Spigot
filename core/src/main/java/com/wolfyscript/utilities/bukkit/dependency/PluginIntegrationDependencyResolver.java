440e2df66236b56474102f0d5cf83c7623cf0e00
WolfyScript <wolfyscript@gmail.com>
2024-05-17 13:44:04 +0200

    Change DependencyResolver to resolve a set of recipes

--- core/src/main/java/com/wolfyscript/utilities/bukkit/dependency/PluginIntegrationDependencyResolver.java
+++ core/src/main/java/com/wolfyscript/utilities/bukkit/dependency/PluginIntegrationDependencyResolver.java
@@ -5,12 +5,13 @@ import com.wolfyscript.utilities.dependency.DependencyResolver;
 import com.wolfyscript.utilities.json.jackson.MissingDependencyException;
 import me.wolfyscript.utilities.api.WolfyUtilCore;
 
-import java.util.Optional;
+import java.util.Collection;
+import java.util.Set;
 
 public class PluginIntegrationDependencyResolver implements DependencyResolver {
 
     @Override
-    public Optional<Dependency> resolve(Object value, Class<?> type) {
+    public Collection<Dependency> resolve(Object value, Class<?> type) {
         var pluginsCompat = WolfyUtilCore.getInstance().getCompatibilityManager().getPlugins();
 
         if (type.isAnnotationPresent(PluginIntegrationDependencyResolverSettings.class)) {
@@ -19,9 +20,9 @@ public class PluginIntegrationDependencyResolver implements DependencyResolver {
             if (integration == null) {
                 throw new MissingDependencyException("Declared integration dependency '" + annotation.pluginName() + "' not found for type '" + type.getName() + "'!");
             }
-            return Optional.of(new PluginIntegrationDependency(integration));
+            return Set.of(new PluginIntegrationDependency(integration));
         }
 
-        return Optional.empty();
+        return Set.of();
     }
 }
--- core/src/main/java/com/wolfyscript/utilities/dependency/DependencyResolver.java
+++ core/src/main/java/com/wolfyscript/utilities/dependency/DependencyResolver.java
@@ -4,13 +4,13 @@ import com.wolfyscript.utilities.json.jackson.MissingDependencyException;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
+import java.util.Collection;
 import java.util.HashSet;
-import java.util.Optional;
 import java.util.Set;
 
 public interface DependencyResolver {
 
-    Optional<Dependency> resolve(Object value, Class<?> type);
+    Collection<Dependency> resolve(Object value, Class<?> type);
 
     static <T> Set<Dependency> resolveDependenciesFor(T value, Class<? extends T> type) {
         final Set<Dependency> dependencies = new HashSet<>();
@@ -22,7 +22,7 @@ public interface DependencyResolver {
 
                 constructor.setAccessible(true);
                 DependencyResolver resolver = constructor.newInstance();
-                resolver.resolve(value, type).ifPresent(dependencies::add);
+                dependencies.addAll(resolver.resolve(value, type));
 
             } catch (NoSuchMethodException | InvocationTargetException | InstantiationException |
                      IllegalAccessException e) {
@@ -33,6 +33,17 @@ public interface DependencyResolver {
 
         for (Field declaredField : type.getDeclaredFields()) {
             if (declaredField.trySetAccessible()) {
+                // Field type is directly annotated with resolver settings
+                if (declaredField.getType().isAnnotationPresent(DependencyResolverSettings.class)) {
+                    try {
+                        var object = declaredField.get(value);
+                        dependencies.addAll(resolveDependenciesFor(object, object.getClass()));
+                    } catch (IllegalAccessException e) {
+                        throw new MissingDependencyException("Failed to fetch dependencies of type '" + declaredField.getType().getName() + "'!", e);
+                    }
+                }
+
+                // Field type isn't providing dependencies directly, but may propagate dependencies
                 DependencySource dependencySource = declaredField.getAnnotation(DependencySource.class);
                 if (dependencySource != null) {
                     try {
@@ -42,7 +53,7 @@ public interface DependencyResolver {
                                 dependencies.addAll(resolveDependenciesFor(object, object.getClass()));
                             }
                         } else {
-                            Object object = declaredField.get(value);
+                            var object = declaredField.get(value);
                             dependencies.addAll(resolveDependenciesFor(object, object.getClass()));
                         }
                     } catch (IllegalAccessException e) {
