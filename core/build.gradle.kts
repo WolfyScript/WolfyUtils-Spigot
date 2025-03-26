7c1ce07e219429f28f9e46393c95d786e4a3b09e
WolfyScript <wolfyscript@gmail.com>
2025-03-26 13:04:14 +0100

    build: update worldedit/worldguard dependencies

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -7,8 +7,8 @@ plugins {
 
 dependencies {
     compileOnly(group = "de.iani.cubeside", name = "LWC", version = "5.0.16-SNAPSHOT")
-    compileOnly(group = "com.sk89q.worldedit", name = "worldedit-bukkit", version = "7.3.0-SNAPSHOT")
-    compileOnly(group = "com.sk89q.worldguard", name = "worldguard-bukkit", version = "7.1.0-SNAPSHOT")
+    compileOnly(group = "com.sk89q.worldedit", name = "worldedit-bukkit", version = "7.3.10")
+    compileOnly(group = "com.sk89q.worldguard", name = "worldguard-bukkit", version = "7.0.13")
     compileOnly(group = "com.plotsquared", name = "PlotSquared-Core", version = "6.4.0")
     compileOnly(group = "com.plotsquared", name = "PlotSquared-Bukkit", version = "6.4.0")
     compileOnly(group = "com.gmail.nossr50.mcMMO", name = "mcMMO", version = "2.2.030")
